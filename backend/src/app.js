51f69328611d65dd509bf3e209dfcaf2355ac2a7
viniciusodst@gmail.com <viniciusodst@gmail.com>
2024-09-14 08:46:08 -0300

    adicionando cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const router = require("./router");
+
 const app = express();
 
 app.use(express.json()); // Middleware para analisar application/json
 app.use(express.urlencoded({ extended: true })); // Middleware para analisar application/x-www-form-urlencoded
-
+app.use(cors());
 app.use(router);
 
 module.exports = app;
